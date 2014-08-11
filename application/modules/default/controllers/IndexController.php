4357d9dd436e6372a6c3dc46d392640f70ec4ab4
anhnv <anhnv1310@gmail.com>
2014-08-11 21:00:12 +0700

    - Update menu

--- application/modules/default/controllers/IndexController.php
+++ application/modules/default/controllers/IndexController.php
@@ -2,31 +2,13 @@
 
 class IndexController extends Zend_Controller_Action {
 
-    /**
-     * トップページのコントローラークラス
-     * <pre>
-     * </pre>
-     * @author foo
-     * @package /modules/default/controller
-     */
-
-    /**
-     * 初期化処理
-     * <pre>
-     * </pre>
-     */
 //     public function init(){
 //         parent::init();
 //     }
 
-    /**
-     * インデックス
-     * <pre>
-     * </pre>
-     */
     public function indexAction() {
-        $user = new Users();
-        $result = $user->test();
+//         $user = new Users();
+//         $result = $user->test();
         
 //         echo "<pre>";
 //         print_r($result);
