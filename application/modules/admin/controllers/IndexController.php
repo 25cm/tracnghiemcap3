4b6d5294b3e9b6c9034c242f12c3227021a9f04f
anhnv <anhnv@trithucmoi.co>
2014-10-07 12:10:27 +0700

    - Delete temp files

--- application/modules/admin/controllers/IndexController.php
+++ application/modules/admin/controllers/IndexController.php
@@ -1,8 +1,21 @@
 <?php
 
+/**
+ * 
+ * @author AnhNV
+ *
+ */
 class Admin_IndexController extends Controller {
 	
+    /**
+     * indexAction
+     */
 	public function indexAction() {
-		
+	    $auth = Zend_Auth::getInstance();
+	    if (!$auth->hasIdentity()) {
+	        return $this->_redirect('/auth/login');
+	    } else if ($auth->getIdentity()->user_type !== '1') {
+	        return $this->_redirect('/error');
+	    }
 	}
 }
